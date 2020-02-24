38dabeea17a791482ed082ef62f17eb1a7fc26a3
Yan <yanique.andre@gmail.com>
2020-02-23 19:52:53 -0500

    Added global styles file that includes all bootstrap and theme classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import "../styles.scss"
 
-export default () => <div>Hello world!</div>
+export default () => (
+<div class="container text-center">
+    <div class="row">
+        <h1>This is a test</h1>
+    </div>
+</div>
+)
